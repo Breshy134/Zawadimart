817ce892811e64acba056652746c4bed00273edf
Breshy134 <buhoribreshy@gmail.com>
2025-04-08 16:10:28 +0300

    initial commit

--- app/src/main/java/com/breshy/zawadimart/ui/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/breshy/zawadimart/ui/screens/dashboard/DashboardScreen.kt
@@ -1,39 +1,170 @@
 package com.breshy.zawadimart.ui.screens.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.breshy.zawadimart.R
 import com.breshy.zawadimart.ui.theme.newBlue
+import com.breshy.zawadimart.ui.theme.newwhite
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(navController: NavHostController) {
-    Column (modifier = Modifier.fillMaxSize()){
+    Column (modifier = Modifier.fillMaxSize().background(newBlue)){
 
-        //Card
-        Card (
-            modifier = Modifier.fillMaxWidth().height(300.dp),
-            shape = RoundedCornerShape(bottomStart = 60.dp, bottomEnd = 60.dp),
-            colors = CardDefaults.cardColors(newBlue)
-        ){
-            TopAppBar(
-                title = { Text(text = "Dashboard Section") }
-            )
+        //Box
+        Box {
+            //Card
+            Card (
+                modifier = Modifier.fillMaxWidth().height(300.dp),
+                shape = RoundedCornerShape(bottomStart = 60.dp, bottomEnd = 60.dp),
+                colors = CardDefaults.cardColors(newwhite)
+            ){
+                TopAppBar(
+                    title = { Text(text = "Dashboard Section") },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+                        }
+                    },
+                )
+            }
+            //End of Card
+
+            Card (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .align(alignment = Alignment.BottomCenter)
+                    .padding(start = 20.dp,end = 20.dp)
+                    .offset(y = 90.dp)
+
+            ){
+
+            }
+
         }
-        //End of Card
+        //End of Box
+
+        Spacer(modifier = Modifier.height(100.dp))
+
+        //Row1
+        Row (modifier = Modifier.padding(start = 20.dp)){
+            //Card1
+            Card (modifier = Modifier.width(150.dp).height(180.dp)){
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.home),
+                        contentDescription = "shirts",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text(text = "Home", fontSize = 15.sp)
+                }
+            }
+            //End of Card1
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            //Card2
+            Card (modifier = Modifier.width(150.dp).height(180.dp)){
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.home),
+                        contentDescription = "shirts",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text(text = "Home", fontSize = 15.sp)
+                }
+            }
+            //End of Card2
+
+
+        }
+        //End of Row1
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        //Row2
+        Row (modifier = Modifier.padding(start = 20.dp)){
+            //Card1
+            Card (modifier = Modifier.width(150.dp).height(180.dp)){
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.home),
+                        contentDescription = "shirts",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text(text = "Home", fontSize = 15.sp)
+                }
+            }
+            //End of Card1
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            //Card2
+            Card (modifier = Modifier.width(150.dp).height(180.dp)){
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.home),
+                        contentDescription = "shirts",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text(text = "Home", fontSize = 15.sp)
+                }
+            }
+            //End of Card2
+
+
+        }
+        //End of Row2
+
+
     }
 }
 
