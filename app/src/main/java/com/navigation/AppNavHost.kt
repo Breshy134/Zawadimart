742a70a8fda0f2535436f0b0a17a82aeb2ea29e5
Breshy134 <buhoribreshy@gmail.com>
2025-04-23 16:07:57 +0300

    initial commit

--- app/src/main/java/com/navigation/AppNavHost.kt
+++ app/src/main/java/com/navigation/AppNavHost.kt
@@ -1,17 +1,23 @@
 package com.breshy.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.breshy.zawadimart.data.UserDatabase
 import com.breshy.zawadimart.navigation.ROUT_HOME
 import com.breshy.zawadimart.navigation.ROUT_ABOUT
+import com.breshy.zawadimart.navigation.ROUT_ADD_PRODUCT
 import com.breshy.zawadimart.navigation.ROUT_DASHBOARD
+import com.breshy.zawadimart.navigation.ROUT_EDIT_PRODUCT
 import com.breshy.zawadimart.navigation.ROUT_FORM
 import com.breshy.zawadimart.navigation.ROUT_FORM1
 import com.breshy.zawadimart.navigation.ROUT_ITEM
@@ -30,18 +36,22 @@ import com.breshy.zawadimart.ui.screens.dashboard.DashboardScreen
 import com.breshy.zawadimart.ui.screens.home.HomeScreen
 import com.breshy.zawadimart.ui.screens.intent.IntentScreen
 import com.breshy.zawadimart.ui.screens.item.ItemScreen
+import com.breshy.zawadimart.ui.screens.products.AddProductScreen
+import com.breshy.zawadimart.ui.screens.products.EditProductScreen
+import com.breshy.zawadimart.ui.screens.products.ProductListScreen
 import com.breshy.zawadimart.ui.screens.service.ServiceScreen
 import com.breshy.zawadimart.ui.screens.start.StartScreen
 import com.breshy.zawadimart.viewmodel.AuthViewModel
 import com.breshy.zawadimart.viewmodel.ProductViewModel
 
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun AppNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = ROUT_PRODUCT_LIST,
-    //productViewModel: ProductViewModel = ViewModel(),
+    startDestination: String = ROUT_ADD_PRODUCT,
+    productViewModel: ProductViewModel = viewModel(),
 ) {
 
 
@@ -104,7 +114,24 @@ fun AppNavHost(
             }
         }
 
-        //98 -> 115
+        // PRODUCTS
+        composable(ROUT_ADD_PRODUCT) {
+            AddProductScreen(navController, productViewModel)
+        }
+
+        composable(ROUT_PRODUCT_LIST) {
+            ProductListScreen(navController, productViewModel)
+        }
+
+        composable(
+            route = ROUT_EDIT_PRODUCT,
+            arguments = listOf(navArgument("productId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val productId = backStackEntry.arguments?.getInt("productId")
+            if (productId != null) {
+                EditProductScreen(productId, navController, productViewModel)
+            }
+        }
 
     }
 }
